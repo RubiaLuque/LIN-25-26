350d07c6ac74af3c6149a9059d90bf7e0f85625a
RubiaLuque <muxu@ucm.es>
2025-11-16 20:29:58 +0100

    practica 4 parte a y b

---
+++ P4-LIN/P4/ParteA/modlist_safe.c
@@ -0,0 +1,165 @@
+#include <linux/module.h>	/* Requerido por todos los módulos */
+#include <linux/kernel.h>	/* Definición de KERN_INFO */
+#include <linux/proc_fs.h>
+#include <linux/string.h>
+#include <linux/slab.h> //Usar kmalloc y vmalloc con memoria contigua 
+#include <linux/uaccess.h> //Copy from user y copy to user
+#include <linux/spinlock.h> //Uso de spinlocks
+
+
+MODULE_LICENSE("GPL"); 	/*  Licencia del modulo */
+
+#define MAXLEN_W 30
+#define MAXLEN_R 256
+
+
+struct list_head myList; //Nodo fantasma de cabecera de la lista enlazada
+
+//Nodos de la lista
+struct list_item {
+    int data;
+	struct list_head links;
+};
+
+static struct proc_dir_entry *proc_entry;
+
+static LIST_HEAD(myList);
+static DEFINE_RWLOCK(RW_lock); //Define un spinlock
+
+//USO: escribe/elimina en la lista lo que se recibe desde espacio de usuario
+//buf es un puntero al espacio de usuario y len el numero de caracteres almacenados en buf
+static ssize_t modlist_write(struct file *filp, const char __user *buf, size_t len, loff_t *off){
+	int num;
+	int available_space = MAXLEN_W-1;
+	char kbuf[MAXLEN_W];
+	
+	if (len > available_space) {
+		printk(KERN_INFO "Modlist: not enough space!!\n");
+    	return -ENOSPC;
+	}
+	
+	//LEE DEL ESPACIO DE USUARIO Y LO GUARDA EN KBUF
+	if(copy_from_user(&kbuf, buf, len)){
+		return -EFAULT;
+	}
+	
+	kbuf[len] = '\0'; //Terminacion de cadena
+	
+	if(sscanf(kbuf, "add %i", &num)==1){
+		//Se espacio para un nuevo nodo de la lista
+		struct list_item* node = kmalloc(sizeof(struct list_item), GFP_KERNEL);
+		node->data = num;
+        write_lock(&rw_lock); //SECCION CRITICA
+		list_add_tail(&node->links, &myList); //Añade al final de la lista un nodo nuevo con el numero dicho
+        write_unlock(&rw_lock); //FIN SECCION CRITICA
+	}
+	else if(sscanf(kbuf, "remove %i", &num)==1){
+		struct list_head* cur_node=NULL;
+		struct list_head* aux = NULL;
+        write_lock(&rw_lock); //SECCION CRITICA
+		list_for_each_safe(cur_node,aux, &myList){ //esta list for each safe permite eliminar un modo durante el recorrido de la lista
+			struct list_item* item = list_entry(cur_node, struct list_item, links);
+			if(item->data == num){
+				list_del(&item->links);
+				kfree(item);
+			}
+		}
+        write_unlock(&rw_lock); //FIN SECCION CRITICA
+	}
+	else if(strcmp(kbuf,"cleanup\n")==0){ // el \n es por el salto de linea que hace echo
+		struct list_head* cur_node=NULL;
+		struct list_head* aux = NULL;
+        write_lock(&rw_lock); //SECCION CRITICA
+		list_for_each_safe(cur_node,aux, &myList){ //esta list for each safe permite eliminar un modo durante el recorrido de la lista
+			struct list_item* item = list_entry(cur_node, struct list_item, links);
+			list_del(&item->links);
+			kfree(item); //libera la memoria del nodo
+		}
+        write_unlock(&rw_lock); //FIN SECCION CRITICA
+	}
+	else{
+		return -EINVAL;
+	}
+	
+	(*off)+=len;  //Se avanza en el desriptor de fichero
+	return len;
+}
+
+//USO: lee de la lista lo que haya dentro y lo muestra en espacio de usuario
+static ssize_t modlist_read(struct file *filp, char __user *buf, size_t len, loff_t *off){
+	int read_bytes = 0;
+	char kbuf[MAXLEN_R];
+	char *dest = kbuf; //Puntero que recorre kbuf
+	
+	if ((*off) > 0){ /* Tell the application that there is nothing left to read */
+		return 0;
+	}
+	
+	struct list_head* cur_node = NULL;
+    read_lock(&rw_lock); //SECCION CRITICA
+	// Copia los datos de la lista a kbuf
+	list_for_each(cur_node, &myList){
+		struct list_item* item = list_entry(cur_node, struct list_item, links);
+		char aux[10];
+		int aux_chars = sprintf(aux, "%d\n", item->data);
+		if(read_bytes + aux_chars >= MAXLEN_R-1){
+			printk(KERN_INFO "Buffer overflow!");
+            read_unlock(&rw_lock); //FIN SECCION CRITICA
+			return -ENOSPC; //OVERFLOW
+		}
+
+		int num_chars = sprintf(dest, "%d\n", item->data);
+		
+		dest+=num_chars; //Avanza el numero de caracteres leidos
+		read_bytes+=num_chars; //Numero de caracteres leidos en total
+		
+	}
+    read_unlock(&rw_lock); //FIN SECCION CRITICA
+	
+	if (len<read_bytes)
+		return -ENOSPC; //No hay espacio suficiente en el buffer de usuario
+	
+	if(copy_to_user(buf, kbuf, read_bytes))
+		return -EINVAL; //Error al copiar a espacio de usuario
+	
+	(*off)+=len; //Avanza el descriptor de fichero
+	return read_bytes;
+	
+}
+
+/* Declaración de funciones init y exit */
+static const struct proc_ops proc_entry_fops = {
+	.proc_read = modlist_read,
+	.proc_write = modlist_write,    
+};
+
+/* Función que se invoca cuando se carga el módulo en el kernel */
+int modulo_lin_init(void)
+{
+	int ret = 0;
+	proc_entry = proc_create( "modlist_safe", 0666, NULL, &proc_entry_fops);
+	if (proc_entry == NULL) {
+		ret = -ENOMEM;
+		printk(KERN_INFO "Modlist Safe: Can't create /proc entry\n");
+	} else {
+		printk(KERN_INFO "Modlist Safe: Module loaded\n");
+	}
+	
+	return ret;
+}
+
+/* Función que se invoca cuando se descarga el módulo del kernel */
+void modulo_lin_clean(void)
+{
+    //Comprobar que no se está accediendo al modulo antes de eliminarlo
+    if(module_refcount(THIS_MODULE) != 0){
+        printk(KERN_INFO "Modlist Safe: Module is busy, can't be removed\n");
+        return;
+    }
+	remove_proc_entry("modlist_safe", NULL);
+	printk(KERN_INFO "Modlist Safe: Module unloaded.\n");
+}
+
+module_init(modulo_lin_init);
+module_exit(modulo_lin_clean);
+
---
+++ P4-LIN/P4/ParteB/prod_cons.c
@@ -0,0 +1,251 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/string.h>
+#include <linux/vmalloc.h>
+#include <linux/uaccess.h>
+#include <linux/jiffies.h>
+#include <linux/wait.h>
+#include <linux/version.h>
+#include <linux/kfifo.h>
+#include <linux/semaphore.h>
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6,2,1)
+#define __cconst__ const
+#else
+#define __cconst__ 
+#endif
+
+MODULE_LICENSE("GPL");
+
+#define DEVICE_NAME "prodcons" /* Dev name as it appears in /proc/devices   */
+#define CLASS_NAME "cool"
+
+#define MAX_ITEMS_CBUF 4
+#define MAX_CHARS_KBUF 20
+
+/*
+ * Global variables are declared as static, so are global within the file.
+ */
+
+static dev_t start;
+static struct cdev* chardev = NULL;
+static struct class* class = NULL;
+static struct device* device = NULL;
+
+//Variables globales específicas de productor-consumidor
+struct kfifo cbuf;
+
+static struct semaphore mtx;
+static struct semaphore huecos;
+static struct semaphore elementos;
+
+//PRODUCTOR --> AÑADE ELEMENTOS AL BUFFER CIRCULAR
+static ssize_t prodcons_write(struct file *filp, const char __user *buf, size_t len, loff_t *off) {
+    char kbuf[MAX_CHARS_KBUF +1];
+    int val = 0;
+
+    if (len > MAX_CHARS_KBUF) {
+        printk(KERN_INFO "prod_cons: not enough space!!\n");
+        return -ENOSPC;
+    }
+
+    if (copy_from_user(&kbuf, buf, len)) {
+        return -EFAULT;
+    }
+
+    if (sscanf(kbuf, "%d", &val) != 1) {
+        printk(KERN_ALERT "Invalid argument\n");
+        return -EINVAL;
+    }
+
+    //---SECCION CRITICA---
+
+    if(down_interruptible(&mtx)){
+        up(&huecos);
+        return -EINTR;
+    }
+
+    //Añade un entero al buffer circular
+    kfifo_in(&cbuf, &val, sizeof(int));
+
+    //---FIN SECCION CRITICA---
+
+    up(&mtx);
+    up(&elementos);
+
+    (*off) += len;
+
+    return len;
+}
+
+//CONSUMIDOR --> LEE ELEMENTOS DEL BUFFER CIRCULAR
+static ssize_t prodcons_read(struct file *filp, char __user *buf, size_t len, loff_t *off) {
+    char kbuf[MAX_CHARS_KBUF +1];
+    int nr_bytes = 0;
+    int val;
+
+    if((*off)>0)
+        return 0;
+        
+    //---SECCION CRITICA---
+
+    if(down_interruptible(&mtx)){
+        up(&elementos);
+        return -EINTR;
+    }
+
+    kfifo_out(&cbuf, &val, sizeof(int));
+
+    //---FIN SECCION CRITICA---
+    up(&mtx);
+    up(&huecos);
+
+    nr_bytes=sprintf(kbuf,"%i\n",val);
+
+    if(copy_to_user(buf, kbuf, nr_bytes)){
+        return -EFAULT;
+    }
+
+    (*off) += len; 
+
+    return nr_bytes;
+}
+
+static struct file_operations fops = {
+    .read = prodcons_read,
+    .write = prodcons_write,
+};
+
+
+static char *custom_devnode(__cconst__ struct device *dev, umode_t *mode)
+{
+    if (!mode)
+        return NULL;
+    if (MAJOR(dev->devt) == MAJOR(start))
+        *mode = 0666;
+    return NULL;
+}
+
+int init_prodcons_module( void )
+{
+    int major;    /* Major number assigned to our device driver */
+    int minor;    /* Minor number assigned to the associated character device */
+    int ret;
+
+    if(kfifo_alloc(&cbuf, MAX_ITEMS_CBUF * sizeof(int), GFP_KERNEL)){
+        return -ENOMEM;
+    }
+
+    sema_init(&elementos, 0);
+    sema_init(&huecos, MAX_ITEMS_CBUF);
+    sema_init(&mtx, 1);
+
+    /* Get available (major,minor) range */
+    if ((ret = alloc_chrdev_region (&start, 0, 1, DEVICE_NAME))) {
+        printk(KERN_INFO "Can't allocate chrdev_region()");
+        goto error_alloc_region;
+    }
+
+    /* Create associated cdev */
+    if ((chardev = cdev_alloc()) == NULL) {
+        printk(KERN_INFO "cdev_alloc() failed ");
+        ret = -ENOMEM;
+        goto error_alloc;
+    }
+
+    cdev_init(chardev, &fops);
+
+    if ((ret = cdev_add(chardev, start, 1))) {
+        printk(KERN_INFO "cdev_add() failed ");
+        goto error_add;
+    }
+
+    /* Create custom class */
+    class = class_create(THIS_MODULE, CLASS_NAME);
+
+    if (IS_ERR(class)) {
+        pr_err("class_create() failed \n");
+        ret = PTR_ERR(class);
+        goto error_class;
+    }
+
+    /* Establish function that will take care of setting up permissions for device file */
+    class->devnode = custom_devnode;
+
+    /*Creating device*/
+    device = device_create(class, NULL, start, NULL, DEVICE_NAME);
+
+    if (IS_ERR(device)) {
+        pr_err("Device_create failed\n");
+        ret = PTR_ERR(device);
+        goto error_device;
+    }
+
+    major = MAJOR(start);
+    minor = MINOR(start);
+
+    printk(KERN_INFO "I was assigned major number %d. To talk to\n", major);
+    printk(KERN_INFO "the driver try to cat and echo to /dev/%s.\n", DEVICE_NAME);
+    printk(KERN_INFO "Remove the module when done.\n");
+
+    printk(KERN_INFO "prod_cons: Module loaded.\n");
+
+    return 0;
+
+    error_device:
+    class_destroy(class);
+
+    error_class:
+    /* Destroy chardev */
+    if (chardev) {
+        cdev_del(chardev);
+        chardev = NULL;
+    }
+
+    error_add:
+    /* Destroy partially initialized chardev */
+    if (chardev)
+        kobject_put(&chardev->kobj);
+
+    error_alloc:
+    unregister_chrdev_region(start, 1);
+
+    error_alloc_region:
+    kfifo_free(&cbuf);
+
+    return ret;
+}
+
+
+void exit_prodcons_module( void )
+{
+    if(module_refcount(THIS_MODULE) != 0){
+        printk(KERN_INFO "prod_cons: Module is busy, can't be removed\n");
+        return;
+    }
+
+    if (device)
+        device_unregister(device);
+
+    if (class)
+        class_destroy(class);
+
+    /* Destroy chardev */
+    if (chardev)
+        cdev_del(chardev);
+
+    /*
+    * Release major minor pair
+    */
+    unregister_chrdev_region(start, 1);
+
+    kfifo_free(&cbuf);
+
+    printk(KERN_INFO "prod_cons: Module unloaded.\n");
+}
+
+
+module_init( init_prodcons_module );
+module_exit( exit_prodcons_module );
